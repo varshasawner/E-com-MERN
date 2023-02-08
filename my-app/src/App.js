089818024a65de22988198c8c87fc2da63714940
varsha <varshasawner9@gmail.com>
2023-02-08 13:13:09 +0530

    implemented tailwind css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1 className='text-xl text-[orange] font-bold'>Hello, Using Tailwind CSS</h1>
     </div>
   );
 }
