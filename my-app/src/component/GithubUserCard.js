8c57c5d5d2adeac6a24b19d378d45396968fec2e
varsha Sawner <varshasawner9@gmail.com>
2023-02-23 18:21:05 +0530

    added image link

--- my-app/src/component/GithubUserCard.js
+++ my-app/src/component/GithubUserCard.js
@@ -4,6 +4,7 @@ export default function GithubUserCard(){
     const [user, setUser] = useState("");
     const [userName, setUserName] = useState("");
     const [userImage, setUserImage] = useState("");
+    const [userlogin, setUserLogin] = useState("");
     // const [userRepo, setUserRepo] = useState([]);
 
 
@@ -15,10 +16,13 @@ export default function GithubUserCard(){
             // console.log(res);
             setUserName(res.name)
             setUserImage(res.avatar_url)
+            setUserLogin(res.login)
             // setUserRepo(res.repos);
             // console.log(userRepo)
         });
     }
+
+    let a = `https://www.github.com/${userlogin}`
 
 
     return (
@@ -26,12 +30,13 @@ export default function GithubUserCard(){
             <h1>Add Git Hub Users</h1>
             <input type="text" onChange={(e) => setUser(e.target.value)} value={user}/><button onClick={addUser}>Add</button>
             <div className="card">
-                <img src={userImage} />
+                <a href={a}><img src={userImage} /></a>
+                
                 <p>{userName}</p>
                 {/* <ol>
                     {
                 userRepo.map((i)=>{
-                    <li>{i[0]}</li>
+                    <li>{i[0].id}</li>
                 })
                 }</ol> */}
             </div>
