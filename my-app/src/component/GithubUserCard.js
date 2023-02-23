02771c70c9e640673e1cbb4232d7eb03b4f3215f
varsha Sawner <varshasawner9@gmail.com>
2023-02-23 17:58:11 +0530

    changes

--- my-app/src/component/GithubUserCard.js
+++ my-app/src/component/GithubUserCard.js
@@ -2,10 +2,22 @@ import { useState } from "react"
 
 export default function GithubUserCard(){
     const [user, setUser] = useState("");
+    const [userName, setUserName] = useState("");
+    const [userImage, setUserImage] = useState("");
+    // const [userRepo, setUserRepo] = useState([]);
+
+
     const addUser = () => {
         // console.log(`https://api.github.com/users/${user}`)
         fetch(`https://api.github.com/users/${user}`)
-        .then((res)=>{ console.log(res.url) });
+        .then((res)=>{ return (res.json()) })
+        .then((res)=>{ 
+            // console.log(res);
+            setUserName(res.name)
+            setUserImage(res.avatar_url)
+            // setUserRepo(res.repos);
+            // console.log(userRepo)
+        });
     }
 
 
@@ -14,8 +26,14 @@ export default function GithubUserCard(){
             <h1>Add Git Hub Users</h1>
             <input type="text" onChange={(e) => setUser(e.target.value)} value={user}/><button onClick={addUser}>Add</button>
             <div className="card">
-                <img src="" />
-                <p>UserName</p>
+                <img src={userImage} />
+                <p>{userName}</p>
+                {/* <ol>
+                    {
+                userRepo.map((i)=>{
+                    <li>{i[0]}</li>
+                })
+                }</ol> */}
             </div>
         </>
     )
