215f282ce1d03fddf2454da9fdbd982c657c0528
varsha Sawner <varshasawner9@gmail.com>
2023-02-24 19:01:08 +0530

    changes

--- my-app/src/component/Cart.js
+++ my-app/src/component/Cart.js
@@ -1,14 +1,43 @@
-export default function Cart(){
+import { useEffect, useState } from "react"
 
-    fetch("https://fakestoreapi.com/products")
-    .then((res)=>{ return res.json()})
-    .then((res)=>{ console.log(res)})
+export default function Cart() {
+    const [product, setProduct] = useState([]);
+    const [cartNumber, setCartNumber] = useState(0);
+
+    useEffect(()=>{
+        fetch("https://fakestoreapi.com/products")
+        .then((res) => { return res.json() })
+        .then((res) => {
+            setProduct(res);
+            console.log(res)
+        })
+    }, [])
+
+
+    const addItem = (e) => {
+        setCartNumber(cartNumber+1)
+    }
+   
 
     return (
         <>
             <h1 align="center">Shopping Cart</h1>
-            <p>&#128722;</p>
-            
+            <p>&#128722; <span>{cartNumber}</span></p>
+            <main>
+                {
+                    product.map((items,index) => {
+                        return (
+                            <div>
+                                <img src={items.image} height={150} width={150} />
+                                <p>{items.title}</p>
+                                <p><b>{items.price}</b></p>
+                                <button onClick={()=>addItem(index)}>Add to Cart</button>
+                            </div>
+                        )
+                    })
+                }
+            </main>
+
         </>
     )
 }
